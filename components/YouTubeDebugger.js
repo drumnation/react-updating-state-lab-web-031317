ffb831da7ecbbe40de4686ccca66250ea3829e9b
drumnation <davidmieloch@gmail.com>
2017-05-16 15:57:21 -0400

    completed lab without learn tests

---
+++ components/YouTubeDebugger.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+    this.handleChangeBitrate = this.handleChangeBitrate.bind(this)
+    this.handleChangeResolution = this.handleChangeResolution.bind(this)
+  }
+
+  // Using Object Assign
+
+  // handleChangeBitrate() {
+  //   this.setState({
+  //     settings: Object.assign({}, this.state.settings, {
+  //       bitrate: 12
+  //     }),
+  //   });
+  // }
+
+  // handleChangeResolution() {
+  //   this.setState({
+  //     settings: Object.assign({}, this.state.settings, {
+  //       video: Object.assign({}, this.state.settings.video, {
+  //         resolution: '720p'
+  //       })
+  //     }),
+  //   });
+  // }
+
+  // Using Object Rest Spread Transformation
+
+  handleChangeBitrate() {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+          bitrate: 12,
+      },
+    })
+  }
+  handleChangeResolution() {
+
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.video,
+          resolution: '720p',
+        },
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button className='bitrate' onClick={ this.handleChangeBitrate }>Change Bitrate</button>
+        <button className='resolution' onClick={ this.handleChangeResolution }>Change Resolution</button>
+      </div>
+    );
+  }
+}
